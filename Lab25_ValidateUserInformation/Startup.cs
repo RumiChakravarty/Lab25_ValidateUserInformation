757bdd64e55894f7397e9cd4ec8abca707ad9497
RumiChakravarty <rumi.sarma.c@gmail.com>
2016-10-03 13:37:05 -0400

    Add project files.

---
+++ Lab25_ValidateUserInformation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab25_ValidateUserInformation.Startup))]
+namespace Lab25_ValidateUserInformation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
